cf706c2217391452d363fad1ced1fbe1105dc07a
gbstorey <gbstorey@asu.edu>
2023-04-08 18:49:21 -0700

    Added new page for new reviews and completed card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,40 +7,21 @@ import { db } from "../../firebaseConfig"
 
 const dbInstance = collection(db, 'reviews')
 
-const submissionHandler = () => {
-}
-
 const Page: NextPageWithLayout = ({reviews}) => {
     return (
         <div className={"h-screen text-center text-2xl px-3"}>
             <h2 className={"font-semibold text-cw_brown py-4"}>
                 Independent, authentic reviews written by coffee enthusiasts like you.
             </h2>
-            <button className={"font-semibold bg-cw_orange px-4 py-1 rounded-xl text-cw_brown drop-shadow-md " +
-                "transition-all ease-in-out duration-300 hover:bg-cw_yellow border"}>
+            <a
+                href={"/new_review"}
+                className={"font-semibold bg-cw_orange px-4 py-1 rounded-xl text-cw_brown drop-shadow-md " +
+                "transition-all ease-in-out duration-300 hover:bg-cw_yellow border cursor-pointer"}>
                 Submit a Review
-            </button>
+            </a>
             {reviews.map((item, num) => {
-                return <Card key={num} numStars={item.numStars} name={item.name} desc={item.desc}/>
+                return <Card key={num} {...item} />
             })}
-            <div className={"w-full text-left text-sm bg-white border border-black rounded p-2"}>
-                <form onSubmit={submissionHandler} method={"POST"}
-                className={""}
-                >
-                    <label className={"block py-2"} htmlFor={"name"}>Name</label>
-                    <input className={"block shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"} name={"name"} placeholder={"Joe Schmoe"}/>
-                    <label className={"block py-2"} htmlFor={"desc"}>Your Thoughts</label>
-                    <textarea className={"block shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"} name={"desc"} placeholder={"Write your review here!"}/>
-                    <label className={"block py-2"} htmlFor={"numStars"}>Rating</label>
-                    <input className={"w-1/2 h-2 rounded-lg appearance-none cursor-pointer dark:bg-cw_orange"}
-                           name={"numStars"} type={"range"} min={0} step={1} max={5}
-                    />
-                    <label className={"block py-2"} htmlFor={"numStars"}>Acidity</label>
-                    <input className={"w-1/2 h-2 rounded-lg appearance-none cursor-pointer dark:bg-cw_orange"}
-                           name={"numStars"} type={"range"} min={0} step={1} max={5}
-                    />
-                </form>
-            </div>
         </div>
     )
 }
---
+++ src/pages/new_review.tsx
@@ -0,0 +1,100 @@
+import {NextPageWithLayout} from "@/pages/_app";
+import {ReactElement, useState} from "react";
+import Layout from "@/UI/Layout";
+import {addDoc, collection, DocumentData} from 'firebase/firestore'
+
+import { db } from "../../firebaseConfig"
+
+const dbInstance = collection(db, 'reviews')
+
+const Page: NextPageWithLayout = () => {
+    const initialState = {
+        name: '', desc: '', numStars: 5, acidity: 5, aroma: 5, body: 5, flavor: 5, sweetness: 5, timestamp: Date.now()
+    }
+    const [inputs, setInputs] = useState(initialState);
+    const submissionHandler = (e) => {
+        setInputs(values => ({...values, timestamp: Date.now()}))
+        e.preventDefault()
+        addDoc(dbInstance, {
+        ...inputs as DocumentData,
+        }).then(() => {
+            setInputs(() => (initialState))
+            alert("Review submitted!")
+        })
+    }
+    const changeHandler = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    }
+    return (
+        <div className={"w-1/2 text-left text-sm bg-white border border-black rounded p-2"}>
+            <form onSubmit={submissionHandler} method={"POST"}>
+                <label className={"flex-1 block py-2"} htmlFor={"name"}>
+                    Name
+                </label>
+                <input name={"name"} placeholder={"Joe Schmoe"} onChange={changeHandler} value={inputs.name}
+                       className={"block shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"}
+                />
+                <label className={"block py-2"} htmlFor={"desc"}>
+                    Your Thoughts
+                </label>
+                <textarea name={"desc"} placeholder={"Write your review here!"} onChange={changeHandler} value={inputs.desc}
+                          className={"block shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"}
+                />
+                <label className={"block py-2"} htmlFor={"acidity"}>
+                    Acidity
+                </label>
+                <input name={"acidity"} type={"range"} min={0} step={1} max={5} value={inputs.acidity} onChange={changeHandler}
+                       className={"w-full h-2 rounded-lg appearance-none cursor-pointer bg-orange-200"}
+                />
+                <label className={"block py-2"} htmlFor={"aroma"}>
+                    Aroma
+                </label>
+                <input name={"aroma"} type={"range"} min={0} step={1} max={5} value={inputs.aroma} onChange={changeHandler}
+                    className={"w-full h-2 rounded-lg appearance-none cursor-pointer bg-orange-300"}
+                />
+                <label className={"block py-2"} htmlFor={"body"}>
+                    Body
+                </label>
+                <input name={"body"} type={"range"} min={0} step={1} max={5} value={inputs.body} onChange={changeHandler}
+                    className={"w-full h-2 rounded-lg appearance-none cursor-pointer bg-orange-400"}
+                />
+                <label className={"block py-2"} htmlFor={"flavor"}>
+                    Flavor
+                </label>
+                <input name={"flavor"} type={"range"} min={0} step={1} max={5} value={inputs.flavor} onChange={changeHandler}
+                    className={"w-full h-2 rounded-lg appearance-none cursor-pointer bg-orange-500"}
+                />
+                <label className={"block py-2"} htmlFor={"sweetness"}>
+                    Sweetness
+                </label>
+                <input name={"sweetness"} type={"range"} min={0} step={1} max={5} value={inputs.sweetness} onChange={changeHandler}
+                    className={"w-full h-2 rounded-lg appearance-none cursor-pointer bg-orange-600"}
+                />
+                <label className={"block py-2"} htmlFor={"numStars"}>
+                    Rating
+                </label>
+                <input name={"numStars"} type={"range"} min={0} step={1} max={5} value={inputs.numStars} onChange={changeHandler}
+                    className={"w-full h-2 rounded-lg appearance-none cursor-pointer bg-orange-700"}
+                />
+                <div className={"text-center"}>
+                    <button className={"font-semibold bg-cw_orange px-4 py-1 my-2 rounded-xl text-cw_brown drop-shadow-md " +
+                        "transition-all ease-in-out duration-300 hover:bg-cw_yellow border items-center"}>
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+Page.getLayout = function getLayout(page: ReactElement) {
+    return (
+        <Layout>
+            {page}
+        </Layout>
+    )
+}
+
+export default Page
